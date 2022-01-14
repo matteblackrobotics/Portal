e3bd5157a634fcf3702a731b4639ff0db2974ddb
matteblackrobotics <mkpfeiffer202@gmail.com>
2022-01-14 15:35:37 -0500

    new files

---
+++ PrintOut.h
@@ -0,0 +1,51 @@
+#ifndef PrintOut_h
+#define PrintOut_h
+
+#include <Arduino.h>
+#include <math.h>
+#include "Lamp.h"
+
+void printInt(String a, int b)
+{
+  Serial.print(" ");
+  Serial.print(a);
+  Serial.print(": ");
+  Serial.print(b); 
+  Serial.print("  ");
+}
+
+
+void printFloat(String a, float b)
+{
+  Serial.print(" ");
+  Serial.print(a);
+  Serial.print(": ");
+  Serial.print(b); 
+  Serial.print("  ");
+}
+
+
+void printBool(String a, bool b)
+{
+  if(b == true)
+  {
+    Serial.print(" ");
+    Serial.print(a);
+    Serial.print(": true ");
+  }
+
+  if(b == false)
+  {
+    Serial.print(" ");
+    Serial.print(a);
+    Serial.print(": false ");
+  }
+}
+
+void printLn()
+{
+  Serial.println(" ");
+}
+
+  
+#endif
